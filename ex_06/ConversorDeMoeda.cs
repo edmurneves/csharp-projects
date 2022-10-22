2265cea515c4becc990ac1c887dbcd0f6dd6478f
Edmur <edmurneves@gmail.com>
2022-10-22 17:58:54 -0300

    Fix: Criada constante estatica Iof

--- ex_06/ConversorDeMoeda.cs
+++ ex_06/ConversorDeMoeda.cs
@@ -5,7 +5,8 @@ namespace Course;
 
 class ConversorDeMoeda {
 
+    public static double Iof = 6.0;
     public static double Conversor(double cotacao, double qte) {
-        return qte * cotacao * 1.06;
+        return qte * cotacao * ( 1 + Iof / 100.0);
     }
 }
