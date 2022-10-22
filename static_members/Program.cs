138cdf3937e469f516569f2c00c3c4347bf54ce3
Edmur <edmurneves@gmail.com>
2022-10-22 17:16:58 -0300

    Separação dos metodos

--- static_members/Program.cs
+++ static_members/Program.cs
@@ -3,27 +3,20 @@ using System.Globalization;
 
 namespace Course;
 
-class Program {
-
-    static double Pi = 3.14;
+class Program {  
     static void Main(string[] args) {
+
+       Calculadora calc = new Calculadora();
 
        Console.Write("Entre o valor do raio: ");
        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-       double circ = Circunferencia(raio);
-       double volume = Volume(raio);
+       double circ = calc.Circunferencia(raio);
+       double volume = calc.Volume(raio);
 
        Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
        Console.WriteLine("volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-       Console.WriteLine("Valor de Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+       Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
     }
-    
-    static double Circunferencia(double r) {
-        return 2.0 * Pi * r;
-    }
-
-    static double Volume(double r) {
-        return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
-    }
+        
 }
