31c9b07c6bbf21d2bf5f01655b3c51ac5bdedcbb
Edmur <edmurneves@gmail.com>
2022-10-22 19:47:47 -0300

    Encapsulamento manual

---
+++ encapsulamento-manual/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course;
+class Program
+{   
+    static void Main(string[] args)
+    { 
+        Produto p = new Produto("TV", 500.00, 10);
+
+        p.SetNome("15");
+
+        Console.WriteLine(p.GetNome());    
+            
+    }
+
+
+}
