31b91964b9e7c50868358d179172481d688e0aca
Edmur <edmurneves@gmail.com>
2022-10-24 09:10:25 -0300

    auto-properties

--- auto-properties/Produto.cs
+++ auto-properties/Produto.cs
@@ -5,14 +5,17 @@ namespace Course;
 class Produto
 {
     private string _nome;
-    private double _preco;
-    private int _quantidade;
+    public double Preco {get; private set;}
+    public int Quantidade { get; private set; }
+
+    public Produto() {
+    }  
 
     public Produto(string nome, double preco, int quantidade)
     {
         _nome = nome;
-        _preco = preco;
-        _quantidade = quantidade;
+        Preco = preco;
+        Quantidade = quantidade;
     }
 
     public string Nome {
@@ -24,27 +27,18 @@ class Produto
         }
     }
 
-    public double Preco {
-        get { return _preco;}
-    }
-
-    public int Quantidade {
-        get { return _quantidade;}
-    }
-
-
     public double ValorTotalEmEstoque()
     {
-        return _preco * _quantidade;
+        return Preco * Quantidade;
     }
     public void AdicionarProdutos(int quantidade)
     {
-        _quantidade += quantidade;
+        Quantidade += quantidade;
     }
 
         public void RemoverProdutos(int quantidade)
     {
-        _quantidade -= quantidade;
+        Quantidade -= quantidade;
     }
 
 
@@ -52,9 +46,9 @@ class Produto
     {
         return _nome 
         + ", $ " 
-        + _preco.ToString("F2", CultureInfo.InvariantCulture)
+        + Preco.ToString("F2", CultureInfo.InvariantCulture)
         + ", "
-        + _quantidade
+        + Quantidade
         + " unidades, Total: $ "
         + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
     }
