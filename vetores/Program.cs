e58bde71dc702b11cb4838f9705778835aa97fd5
Edmur <edmurneves@gmail.com>
2022-10-26 09:44:13 -0300

    Exercicio sobre vetores

---
+++ vetores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace vetores;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+
+        double avg = sum / n;
+
+        Console.WriteLine("Média de altura: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+}
---
+++ vetores2/Product.cs
@@ -0,0 +1,11 @@
+namespace vetores2;
+
+class Product {
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public Product(string name, double price) {
+        Name = name;
+        Price = price;
+    }
+}
